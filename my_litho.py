a6d6c0fa5b8dbc813f53d823b045555b4fbd7fda
tahsin <tahsinanika1410@gmail.com>
2019-07-23 16:00:04 +0900

    changed and deleted unnecessary files after pulling from origin master

--- my_litho.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from ctypes import *
-litho = CDLL("./pythonlib/litholib.so")
-import numpy as np
-#import pandas as pd
-
-
-litho.clear_design_space();
-litho.read_layout("benchmark/M1_test9.glp");  #for now fixed
